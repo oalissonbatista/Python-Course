3ee308713b0b19ca9b42d6ee89308c980aa19a5b
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-06 14:12:43 -0300

    alteração no exercício 024

--- Curso de Python/024.py
+++ Curso de Python/024.py
@@ -1,8 +1,8 @@
 from xmlrpc.client import boolean
 # verificação de nome
-cidade =str(input("Digite o nome da cidade: "))
+cidade = str(input("Digite o nome da cidade: "))
 cidade.capitalize()
 separado = cidade.split()
 procurando =(separado[0].upper() == "SANTO")
-
+print(separado[0].upper())
 print("O nome da cidade começa com Santo?\n{}".format(boolean(procurando)))
