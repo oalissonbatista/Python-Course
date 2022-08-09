ba7d3cb1f07c68452967f516670c38c82f09532b
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-09 13:50:31 -0300

    changes

--- Curso de Python/033.py
+++ Curso de Python/033.py
@@ -2,13 +2,24 @@ n1 = float(input("Digite um número:"))
 n2 = float(input("Digite um número:"))
 n3 = float(input("Digite um número:"))
 
-if n1 > n2 > n3:
+if n1 >= n2 >= n3:
     print("{} é o maior".format(n1))
-elif n1 > n3 > n2:
+    print("{} é o menor".format(n3))
+elif n1 >= n3 >= n2:
     print("{} é o maior".format(n1))
-elif n2 > n1 > n3: 
+    print("{} é o menor".format(n2))
+elif n2 >= n1 >= n3: 
     print("{} é o maior".format(n2))
-elif n2 > n3 > n1:
+    print("{} é o menor".format(n3))
+elif n2 >= n3 >= n1:
     print("{} é o maior".format(n2))
-else: 
+    print("{} é o menor".format(n1))
+elif n3 >= n1 >= n2:
     print("{} é o maior".format(n3))
+    print("{} é o menor".format(n2))
+elif n3 >= n2 >= n1:
+    print("{} é o maior".format(n3))
+    print("{} é o menor".format(n1))
+
+else: 
+    print("erro")
