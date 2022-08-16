fa8c856b96e6947655a050a20bb7cd743291ed75
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-16 20:00:34 -0300

    Melhorias no código

--- Curso de Python/030.py
+++ Curso de Python/030.py
@@ -1,6 +1,9 @@
 #numero par ou impar
+from time import sleep
 numero = float(input("Digite um número:"))
+print("PROCESSANDO...")
+sleep(2)
 if (numero%2) == 0:
-    print("{} é par".format(numero))
+    print("{} é PAR".format(numero))
 else: 
-    print("{} é ímpar".format(numero))
+    print("{} é ÍMPAR".format(numero))
--- Curso de Python/031.py
+++ Curso de Python/031.py
@@ -1,5 +1,6 @@
 #Taximetro
 distancia = float(input("Qual a distância da viagem?"))
+# preço= distancia * 0.50 if distancia <=200 else distancia * 0.45
 if distancia <= 200:
     valor = 0.50*distancia
     print("O valor da viagem será R${:.2f}".format(valor))
