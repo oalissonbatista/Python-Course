529b0ad16fdf6f2c3cc4112e8ed1702e804114b1
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-16 19:22:50 -0300

    update

--- Curso de Python/028.py
+++ Curso de Python/028.py
@@ -1,8 +1,7 @@
-import random
 #adivinhando numero
+import random
 n = [0,1,2,3,4,5]
 escolhido = random.choice(n)
-
 chute = int(input("Qual número foi escolhido?"))
 if chute == escolhido:
     print("YOU WIN")
