226f9ff43c2bb79f870073e41360342988dd4747
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-16 19:22:35 -0300

    Update exercício 28

---
+++ Curso de Python/028.1.py
@@ -0,0 +1,12 @@
+#Adivinhando número
+from random import randint
+from time import sleep
+computador = randint(0,5)
+print("Vou pensar em um número de 0 a 5. Tente adivinhar...")
+chute = int(input("Chute um número:"))
+print("PROCESSANDO...")
+sleep(3)
+if chute == computador:
+    print("Parabéns, você acertou")
+else:
+    print("Você errou, o número era: {}".format(computador))
