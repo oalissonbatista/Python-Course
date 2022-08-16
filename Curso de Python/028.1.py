74543b757a0dcd2eaf8aed369377c707d93b8842
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-16 19:25:45 -0300

    Melhorando layout

--- Curso de Python/028.1.py
+++ Curso de Python/028.1.py
@@ -2,7 +2,9 @@
 from random import randint
 from time import sleep
 computador = randint(0,5)
+print("-"*52)
 print("Vou pensar em um número de 0 a 5. Tente adivinhar...")
+print("-"*52)
 chute = int(input("Chute um número:"))
 print("PROCESSANDO...")
 sleep(3)
