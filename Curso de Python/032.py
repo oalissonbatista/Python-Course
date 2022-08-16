aabdab581e84b736ea66bbe3ee50c750b53a5110
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-16 20:00:48 -0300

    Melhorias no código

--- Curso de Python/032.py
+++ Curso de Python/032.py
@@ -1,6 +1,9 @@
 #Ano Bissexto
-ano = int(input(("Digite o ANO:")))
-if (ano%4) == 0:
+from datetime import date
+ano = int(input(("Digite o ANO a ser analisado:")))
+if ano == 0:
+    ano = date.today().year
+if (ano%4 == 0 and ano % 100 != 0 or ano % 400 == 0 ):
     print("{} é bissexto".format(ano))
 else:
     print("{} NÃO é bissexto".format(ano))
