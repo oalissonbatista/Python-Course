910b4b8fadea91c930e29ee89773394d6dae5a7f
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-08-17 09:31:32 -0300

    Updates

--- Curso de Python/034.py
+++ Curso de Python/034.py
@@ -1,12 +1,17 @@
 #Aumento salario
+from time import sleep
 salario = float(input("Digite o salario do funcionario:"))
 if salario > 1250:
     aumento = (10/100)*salario
     novoSalario = salario + aumento
+    print("CALCULANDO AUMENTO...")
+    sleep(2)
     print("Com aumento de R${:.2f} o salário passará a ser de R${:.2f}".format(aumento, novoSalario))
 else:
     aumento = (15/100)*salario
     novoSalario = salario + aumento
+    print("CALCULANDO AUMENTO...")
+    sleep(2)
     print("Com aumento de R${:.2f} o salário passará a ser de R${:.2f}".format(aumento,novoSalario))
 print("----Fim----")
     
